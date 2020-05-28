b86961c46cb7f48f768a68a26dbb7609c01ca2d2
NickFessel <nfessel@gmail.com>
2020-05-28 18:43:32 -0400

    Made dataset generic.

--- BlazorTabulator/wwwroot/blazortabulator.js
+++ BlazorTabulator/wwwroot/blazortabulator.js
@@ -6,7 +6,7 @@
         });
      },
     updateTable: function (elem, tableData) {
-        var tabulatorTable = Tabulator.prototype.findTable("#tabulatorTable")[0];
-        tableData.people.forEach((t) => { tabulatorTable.addRow(t)}); // todo: allow any data, not just "people"
+       var tabulatorTable = Tabulator.prototype.findTable("#tabulatorTable")[0];
+       tableData.dataset.forEach((t) => { tabulatorTable.addRow(t)});
     }
 };
